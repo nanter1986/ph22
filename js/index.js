57a59751f96880aa36f97e0bfc4aaee5f8d0f921
nanter1986 <nanter1986@gmail.com>
2018-07-29 10:59:25 +0300

    g

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 var game= new Phaser.Game(800,600,Phaser.AUTO);
 
-var Gamestate = {
+//var Gamestate = {
 	preload:function preload(){
 		game.load.image('pic','/assets/robot.png');
-	},
+	}//,
 	create:function create(){
 		var image=game.add.image(0,0,'pic');
-	},
+	}//,
 	update:function update(){
 
 	}
-}
+//}
 
-game.state.add('Gamestate',Gamestate);
-game.state.start('Gamestate');
+//game.state.add('Gamestate',Gamestate);
+//game.state.start('Gamestate');
